e6deb098c2707bbd0e893c22cf888f9158065d4b
Edita Marija <edimcdowl@yahoo.com>
2020-04-15 18:10:33 +0300

    animated fadeIn

--- resources/js/script.js
+++ resources/js/script.js
@@ -37,4 +37,12 @@ $(document).ready(function() {
         }
       });
     });
+    
+    /* Animation on scroll */
+    $(".js--wp-1").waypoint(function(direction) {
+        $(".js--wp-1").addClass("animated fadeIn");
+    }, {
+        offset: '50%'
+    });
+    
 });
